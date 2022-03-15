0d7f74560b6b9c0e67544e9dd760166e1474329a
YvesMor <83225014+YvesMor@users.noreply.github.com>
2022-03-15 14:06:04 -0400

    Creation of System Enums class

---
+++ Assets/YvesDEV/Scripts/SystemEnums.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemEnums
+{
+    // Start is called before the first frame update
+    public enum Partys { None, Ally, Enemy }
+}
