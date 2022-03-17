7ab3fdfcc18a80a7964743ad7d1019313df23879
mariannabell <mariannabelle.doodles@gmail.com>
2022-03-17 11:24:52 -0400

    material offsets done

--- Assets/JulienDEV/Scripts/UI/ButtonLoadScene.cs
+++ Assets/JulienDEV/Scripts/UI/ButtonLoadScene.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ButtonLoadScene : MonoBehaviour
 {
-    [SerializeField] private string _gamePlayScene;
+    //[SerializeField] private string _gamePlayScene;
 
     public void StartButton()
     {
-        SceneManager.LoadScene(_gamePlayScene, LoadSceneMode.Single);
+        //SceneManager.LoadScene(_gamePlayScene, LoadSceneMode.Single);
+
+        Debug.Log("loading game");
+        SceneManager.LoadScene(1);
     }
 }
