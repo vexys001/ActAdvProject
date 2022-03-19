6ad3fbfc9a8445c46fb06ba3baf73ec2027a6cc6
YvesMor <83225014+YvesMor@users.noreply.github.com>
2022-03-19 15:30:03 -0400

    Activator Clean Up + Locks in the main scene

--- Assets/JulienDEV/Scripts/Interactables/Activator.cs
+++ Assets/JulienDEV/Scripts/Interactables/Activator.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour
 {
-    private enum _modes { button, buttonOneShot, buttonSelfDestruct, pressurePlate };
-    [SerializeField] private _modes _Modes;
-
     //Common
     [SerializeField] protected GameObject _target = null;
 
@@ -19,91 +16,6 @@ public class Activator : MonoBehaviour
     {
         _objectRenderer = gameObject.GetComponent<Renderer>();
         _objectRenderer.material.color = _objectColor;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        switch (_Modes)
-        {
-            /*case _modes.button:
-                if (collision.gameObject.tag == _colliderTag)
-                {
-                    OnOff();
-                }
-                break;
-            */
-            /*case _modes.buttonOneShot:
-                if (collision.gameObject.tag == _colliderTag && _buttonOneShotPressed == false)
-                {
-                    OnOff();
-                    _buttonOneShotPressed = true;
-                }
-                break;
-            */
-            /*case _modes.buttonSelfDestruct:
-                if (collision.gameObject.tag == _colliderTag)
-                {
-                    OnOff();
-                    Destroy(gameObject);
-                }
-                break;
-            */
-            /*case _modes.pressurePlate:
-                //
-                break;*/
-        }
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        switch (_Modes)
-        {
-            /*
-            case _modes.button:
-                //
-                break;
-            */
-            /*
-            case _modes.buttonOneShot:
-                //
-                break;
-            
-            case _modes.buttonSelfDestruct:
-                //
-                break;
-
-            /*case _modes.pressurePlate:
-                if (collision.gameObject.tag == _colliderTag && _isPressurePlatePressed == false)
-                {
-                    OnOff();
-                    _isPressurePlatePressed = true;
-                }
-                break;*/
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        switch (_Modes)
-        {
-            /*
-            case _modes.button:
-                //
-                break;
-            
-            case _modes.buttonOneShot:
-                //
-                break;
-
-            case _modes.buttonSelfDestruct:
-                //
-                break;
-
-            /*case _modes.pressurePlate:
-                OnOff();
-                _isPressurePlatePressed = false;
-                break;*/
-        }
     }
 
     protected void OnOff()
