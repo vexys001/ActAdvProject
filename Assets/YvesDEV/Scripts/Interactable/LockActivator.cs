51a69f39e2157264ee31b8afe7d76bd03bb05ea3
YvesMor <83225014+YvesMor@users.noreply.github.com>
2022-03-19 12:54:10 -0400

    Lock start

--- Assets/YvesDEV/Scripts/Interactable/LockActivator.cs
+++ Assets/YvesDEV/Scripts/Interactable/LockActivator.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LockActivator : MonoBehaviour
+public class LockActivator : Activator
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private SystemEnums.KeyColors _requiredColor;
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+
+        if (collision.gameObject.tag == _colliderTag)
+        {
+            Pog pogObject = collision.gameObject.GetComponentInChildren<Pog>();
+            if(pogObject.KeyColor == _requiredColor)
+            {
+                //OnOff();
+                Destroy(gameObject);
+            }
+        }
     }
 }
